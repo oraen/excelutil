76b56661754077e06bff0a800dcee54bed23a6c8
Corki <1543493541@qq.com>
2021-02-23 09:29:42 +0800

    .

--- src/main/java/com/oraen/oxygen/excelutil/Responsibility/AssemblyLine.java
+++ src/main/java/com/oraen/oxygen/excelutil/Responsibility/AssemblyLine.java
@@ -3,21 +3,21 @@ package com.oraen.oxygen.excelutil.Responsibility;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@SuppressWarnings("unchecked")
 public class AssemblyLine<T> {
 
-    private List<WorkNode<T>> assemblyLine = new ArrayList<WorkNode<T>> (8);
+    private List<WorkNode<? super T>> assemblyLine = new ArrayList<WorkNode<? super T>> (8);
 
     public T work(T target, Object... params) throws Exception {
         T re = target;
-        for(WorkNode<T> w : assemblyLine){
-            re = w.execute(re, params);
+        for(WorkNode<? super T> w : assemblyLine){
+            re = (T)(w.execute(re, params));
         }
 
         return re;
     }
 
-    public AssemblyLine<T> addNode(WorkNode<T> workNode){
+    public AssemblyLine<T> addNode(WorkNode<? super T> workNode){
         this.assemblyLine.add(workNode);
         return this;
     }
